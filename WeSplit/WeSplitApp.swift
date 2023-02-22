0683d178602c91cf900900531a6a251abee60176
systemsevenfive <ryanwsaunders@icloud.com>
2023-02-21 19:45:48 -0500

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Ryan Saunders on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
